9e589c61c145377fd61304e4f082f70a9f064f0e
Eliezer <eliesteinbock@gmail.com>
2013-04-11 23:37:15 +0300

    i think todoDAL is done

--- TodoListManager/src/il/ac/huji/todolist/TodoDAL.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoDAL.java
@@ -1,9 +1,12 @@
 package il.ac.huji.todolist;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -38,15 +41,53 @@ public class TodoDAL extends SQLiteOpenHelper {
 	}
 	
 	public boolean update(ITodoItem todoItem) {
-		return false;
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(KEY_TITLE, todoItem.getTitle());
+		values.put(KEY_DUE, todoItem.getDueDate().getTime());
+
+		String where = KEY_TITLE + " = ?";
+		String[] whereArgs = {todoItem.getTitle()};
+		
+		long r = db.update(TABLE_TODO, values, where, whereArgs);
+		db.close();
+		
+		return r != -1;
 	}
 	
 	public boolean delete(ITodoItem todoItem) {
-		return false;
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		String where = KEY_TITLE + " = ?";
+		String[] whereArgs = {todoItem.getTitle()};
+		
+		long r = db.delete(TABLE_TODO, where, whereArgs);
+		db.close();
+		
+		return r != -1;
 	}
 	
-	public List<ITodoItem> all() {
-		return null;
+	@SuppressWarnings("deprecation")
+	public List<ITodoItem> all() {		
+		List<ITodoItem> todoItemList = new ArrayList<ITodoItem>();
+		
+	    // Select All Query
+	    String selectQuery = "SELECT * FROM " + TABLE_TODO;
+	 
+	    SQLiteDatabase db = this.getWritableDatabase();
+	    Cursor cursor = db.rawQuery(selectQuery, null);
+	 
+	    // looping through all rows and adding to list
+	    if (cursor.moveToFirst()) {
+	        do {
+	            Task task = new Task(cursor.getString(1), new Date(cursor.getString(2)));
+	            // Adding contact to list
+	            todoItemList.add(task);
+	        } while (cursor.moveToNext());
+	    }
+		
+		return todoItemList;
 	}
 	
 	@Override
@@ -54,8 +95,7 @@ public class TodoDAL extends SQLiteOpenHelper {
 		db.execSQL("create table " + TABLE_TODO +
 				" ( " + KEY_ID + " integer primary key autoincrement, "
 				+ KEY_TITLE + " text, "
-				+ KEY_DUE + " long);");//TODO should due be long or int?
-
+				+ KEY_DUE + " long);");
 	}
 	
 	@Override
